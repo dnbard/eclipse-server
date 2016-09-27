ed589c49abf38e07c2e6f157e9b028f6a0ba376d
Oleksandr Bardanov <oleksandr.bardanov@avid.com>
2016-09-27 16:45:56 +0300

    added tests for subscriptions, #15

--- core/subscriptions.js
+++ core/subscriptions.js
@@ -1,7 +1,7 @@
 const uuid = require('node-uuid').v4;
 const _ = require('lodash');
 
-const subscriptions = [];
+var subscriptions = [];
 
 exports.createSubscription = function(subscriber, subscribeTo){
     const subscription = {
@@ -15,10 +15,18 @@ exports.createSubscription = function(subscriber, subscribeTo){
     return subscription;
 }
 
+exports.getSubscriptionBySubscriberId = function(subscriberId){
+    return _.filter(subscriptions, s => s.subscriber === subscriberId);
+}
+
 exports.removeSubscriptionById = function(subscriptionId){
     _.remove(subscriptions, s => s.id === subscriptionId);
 }
 
 exports.removeSubscriptionBySubscriberId = function(subscriberId){
     _.remove(subscriptions, s => s.subscriber === subscriberId);
+}
+
+exports.removeAll = function(){
+    subscriptions = [];
 }
---
+++ tests/core/subscriptionsSpec.js
@@ -0,0 +1,74 @@
+const expect = require('chai').expect;
+const Subscriptions = require('../../core/subscriptions');
+const uuid = require('node-uuid').v4;
+
+
+describe('Subscriptions', function(){
+    afterEach(() => {
+        Subscriptions.removeAll();
+    });
+
+    describe('#createSubscription', function(){
+        it('should create new subscription', function(){
+            const subscriber = uuid();
+            Subscriptions.createSubscription(subscriber, uuid());
+
+            expect(Subscriptions.getSubscriptionBySubscriberId(subscriber)).to.have.length(1);
+        });
+    });
+
+    describe('#getSubscriptionBySubscriberId', function(){
+        it('should return subscriptions', function(){
+            const subscriber = uuid();
+            Subscriptions.createSubscription(subscriber, uuid());
+
+            expect(Subscriptions.getSubscriptionBySubscriberId(subscriber)).to.have.length(1);
+        });
+    });
+
+    describe('#removeSubscriptionById', function(){
+        it('should remove subscription', function(){
+            const subscriber = uuid();
+            const subscription = Subscriptions.createSubscription(subscriber, uuid());
+            Subscriptions.removeSubscriptionById(subscription.id);
+
+            expect(Subscriptions.getSubscriptionBySubscriberId(subscriber)).to.have.length(0);
+        });
+
+        it('should not remove subscription if none', function(){
+            Subscriptions.removeSubscriptionById(uuid());
+
+            expect(Subscriptions.getSubscriptionBySubscriberId(uuid())).to.have.length(0);
+        });
+    });
+
+    describe('#removeSubscriptionBySubscriberId', function(){
+        it('should remove subscription', function(){
+            const subscriber = uuid();
+            Subscriptions.createSubscription(subscriber, uuid());
+            Subscriptions.removeSubscriptionBySubscriberId(subscriber);
+
+            expect(Subscriptions.getSubscriptionBySubscriberId(subscriber)).to.have.length(0);
+        });
+
+        it('should not remove subscription if none', function(){
+            Subscriptions.removeSubscriptionBySubscriberId(uuid());
+
+            expect(Subscriptions.getSubscriptionBySubscriberId(uuid())).to.have.length(0);
+        });
+    });
+
+    describe('#removeAll', function(){
+        it('should remove all subscriptions', function(){
+            const subscriber = uuid();
+            Subscriptions.createSubscription(uuid(), uuid());
+            Subscriptions.createSubscription(subscriber, uuid());
+            Subscriptions.createSubscription(subscriber, uuid());
+            Subscriptions.createSubscription(uuid(), uuid());
+            Subscriptions.createSubscription(uuid(), uuid());
+            Subscriptions.removeAll();
+
+            expect(Subscriptions.getSubscriptionBySubscriberId(subscriber)).to.have.length(0);
+        });
+    });
+});
