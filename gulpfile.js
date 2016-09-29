7ee8b295e5943e6d6a2979ddd78254d9260471e2
Alex Bardanov <dnbard@gmail.com>
2016-09-29 23:23:37 +0300

    To run back-end tests on every source code changes, close #16

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const nodemon = require('gulp-nodemon');
+const mocha = require('gulp-mocha');
+
+gulp.task('default', function () {
+    nodemon({
+        tasks: ['test'],
+        delay: 10
+    });
+});
+
+gulp.task('test', function(){
+    return gulp.src('./tests/**/*Spec.js', {
+        read: false
+    }).pipe(mocha({
+        reporter: 'min'
+    }));
+});
