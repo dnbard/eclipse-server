8cabdd2a58c5c45e984e0df4b15de8eefb1c409c
Oleksandr Bardanov <oleksandr.bardanov@avid.com>
2016-09-30 14:25:28 +0300

    minor dependency initialization improvements

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const server = require('http').createServer()
 const express = require('express');
-const app = express();
 
-const config = require('./config');
-const ws = require('./core/ws');
-const middlewares = require('./middlewares');
 const UpdateLoop = require('./core/updateLoop');
 const mongo = require('./core/mongo');
+const ws = require('./core/ws');
+
+const middlewares = require('./middlewares');
+const config = require('./config');
 
 mongo.connect().then(db => {
+    const server = require('http').createServer();
+    const app = express();
+
     server.on('request', app);
     server.listen(config.port, () => {
         console.log(`HTTP Server :: listening on port ${config.port}`);
