6933b0569cc0b8c845c1b16bb58f48dcc1afa71e
Alex Bardanov <dnbard@gmail.com>
2016-10-17 21:35:22 +0300

    mongodb enabled

---
+++ bash/mongo.sh
@@ -0,0 +1,15 @@
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
+sudo apt-get update
+
+sudo apt-get install -y mongodb-org
+
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+
+sudo service mongod start
--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.port = process.env.PORT || 3000;
-exports.mongo = process.env.MONGO || 'mongodb://localhost:27018/eclipse';
+exports.mongo = process.env.MONGO || 'mongodb://localhost:27017/eclipse';
 
 exports.appRoot = __dirname;
