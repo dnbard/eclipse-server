e9756c0ac141fdfe95fa71b266d75221411206c9
Oleksandr Bardanov <oleksandr.bardanov@avid.com>
2016-10-20 16:43:34 +0300

    dev mongo port added to config

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 exports.port = process.env.PORT || 3000;
-exports.mongo = process.env.MONGO || 'mongodb://localhost:27017/eclipse';
-exports.mongoTest = process.env.MONGO_TEST || 'mongodb://localhost:27017/eclipse-test';
+exports._mongoPort = process.env.DEV_MONGO_PORT || 27017;
+exports.mongo = process.env.MONGO || `mongodb://localhost:${exports._mongoPort}/eclipse`;
+exports.mongoTest = process.env.MONGO_TEST || `mongodb://localhost:${exports._mongoPort}/eclipse-test`;
 
 exports.appRoot = __dirname;
