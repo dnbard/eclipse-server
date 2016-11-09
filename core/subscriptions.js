d235ab6ec397bf070a19f00c4f557518fd0a5118
Volodymyr Matvieiev <vl.matveew@gmail.com>
2016-11-10 00:26:04 +0200

    Refactor subscribe storage to optimize read, close #15

--- core/subscriptions.js
+++ core/subscriptions.js
@@ -1,13 +1,17 @@
-const uuid = require('./uuid');
+'use strict';
+
+const debug = require('debug')('subscriptions');
 const _ = require('lodash');
+const uuid = require('./uuid');
 
-var subscriptions = [];
+let subscriptionsBySubscriberMap = new Map();
+let subscriptionsBySubscribeToMap = new Map();
+let subscriptionsByIdMap = new Map();
 
-function toJSON(){
-    return Object.assign({}, this, { ws: undefined });
-}
+exports.createSubscription = function(subscriber, subscribeTo, ws) {
+    let subscriptionsBySubscriber = subscriptionsBySubscriberMap.get(subscriber) || [];
+    let subscriptionsBySubscribeTo = subscriptionsBySubscribeToMap.get(subscribeTo) || [];
 
-exports.createSubscription = function(subscriber, subscribeTo, ws){
     const subscription = {
         id: uuid(),
         subscriber: subscriber,
@@ -16,33 +20,85 @@ exports.createSubscription = function(subscriber, subscribeTo, ws){
         toJSON: toJSON
     };
 
-    subscriptions.push(subscription);
+    subscriptionsBySubscriber.push(subscription);
+    subscriptionsBySubscribeTo.push(subscription);
 
-    console.log(`Subscription (id=${subscription.id}, subscriber=${subscriber}) created`);
+    subscriptionsBySubscriberMap.set(subscriber, subscriptionsBySubscriber);
+    subscriptionsBySubscribeToMap.set(subscribeTo, subscriptionsBySubscribeTo);
+    subscriptionsByIdMap.set(subscription.id, subscription);
+
+    debug(`Subscription (id=${subscription.id}, subscriber=${subscriber}) created`);
 
     return subscription;
 }
 
 exports.getSubscriptionBySubscriberId = function(subscriberId){
-    return _.filter(subscriptions, s => s.subscriber === subscriberId);
+    return (subscriptionsBySubscriberMap.get(subscriberId) || []);
 }
 
 exports.getSubscriptionBySubscribeToId = function(subscribeTo){
-    return _.filter(subscriptions, s => s.subscribeTo === subscribeTo);
+    return (subscriptionsBySubscribeToMap.get(subscribeTo) || []);
 }
 
 exports.removeSubscriptionById = function(subscriptionId){
-    _.remove(subscriptions, s => s.id === subscriptionId);
+    let subscription = subscriptionsByIdMap.get(subscriptionId);
 
-    console.log(`Subscriptions (id=${subscriptionId} removed)`);
+    if (!subscription) {
+        return;
+    }
+
+    let subscriber = subscription.subscriber;
+    let subscribeTo = subscription.subscribeTo;
+    let subscriptionsBySubscriber = subscriptionsBySubscriberMap.get(subscriber);
+    let subscriptionsBySubscribeTo = subscriptionsBySubscribeToMap.get(subscribeTo);
+
+    _.remove(subscriptionsBySubscriber, s => s.id === subscriptionId);
+    _.remove(subscriptionsBySubscribeTo, s => s.id === subscriptionId);
+
+    if (subscriptionsBySubscriber.length === 0) {
+        subscriptionsBySubscriberMap.delete(subscriber);
+    }
+
+    if (subscriptionsBySubscribeTo.length === 0) {
+        subscriptionsBySubscribeToMap.delete(subscribeTo);
+    }
+
+    subscriptionsByIdMap.delete(subscriptionId);
+
+    debug(`Subscriptions (id=${subscriptionId} removed)`);
 }
 
-exports.removeSubscriptionBySubscriberId = function(subscriberId){
-    _.remove(subscriptions, s => s.subscriber === subscriberId);
+exports.removeSubscriptionBySubscriberId = function(subscriberId) {
+    let subscriptionsBySubscriber = subscriptionsBySubscriberMap.get(subscriberId);
 
-    console.log(`Subscriptions (subscriberId=${subscriberId} removed)`);
+    if (!subscriptionsBySubscriber) {
+        return;
+    }
+
+    subscriptionsBySubscriber.forEach(subscription => {
+        let subscribeTo = subscription.subscribeTo;
+        let subscriptionsBySubscribeTo = subscriptionsBySubscribeToMap.get(subscribeTo);
+
+        _.remove(subscriptionsBySubscribeTo, s => s.subscriber === subscriberId);
+
+        if (subscriptionsBySubscribeTo.length === 0) {
+            subscriptionsBySubscribeToMap.delete(subscribeTo);
+        }
+
+        subscriptionsByIdMap.delete(subscription.id);
+    });
+
+    subscriptionsBySubscriberMap.delete(subscriberId);
+
+    debug(`Subscriptions (subscriberId=${subscriberId} removed)`);
 }
 
-exports.removeAll = function(){
-    subscriptions = [];
+exports.removeAll = function() {
+    subscriptionsBySubscriberMap.clear();
+    subscriptionsBySubscribeToMap.clear();
+    subscriptionsByIdMap.clear();
+}
+
+function toJSON(){
+    return Object.assign({}, this, { ws: undefined });
 }
